5a269ba63cbc00950849c83e61a56520a2c3cdae
Nathan Kot <me@nathankot.com>
2014-08-08 23:13:16 +1200

    osx settings.

--- modules/people/manifests/nathankot.pp
+++ modules/people/manifests/nathankot.pp
@@ -6,6 +6,16 @@ class people::nathankot {
   include people::nathankot::karabiner
   include fish
 
+  # osx
+  include osx::global::disable_key_press_and_hold
+  include osx::global::enable_keyboard_control_access
+  include osx::global::disable_remote_control_ir_receiver
+  include osx::global::tap_to_click
+  include osx::dock::autohide
+  include osx::disable_app_quarantine
+  include osx::no_network_dsstores
+  include osx::keyboard::capslock_to_control
+
   # $my       = "${home}/my"
   # $dotfiles = "${my}/dotfiles"
   #
