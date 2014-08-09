b0cb47ac721fbcff62db12f28a7240e371e7573a
Nathan Kot <me@nathankot.com>
2014-08-09 15:22:03 +1200

    Explicit gem versions.

--- modules/people/manifests/nathankot.pp
+++ modules/people/manifests/nathankot.pp
@@ -97,36 +97,37 @@ class people::nathankot {
   # Gems
   ruby_gem { 'lunchy':
     gem          => 'lunchy',
+    version      => '~> 0.8',
     ruby_version => '*'
   }
 
   ruby_gem { 'mailcatcher':
     gem          => 'mailcatcher',
+    version      => '~> 0.5',
     ruby_version => '*'
   }
 
   ruby_gem { 'ghi':
     gem          => 'ghi',
+    version      => '~> 0.9',
     ruby_version => '*'
   }
 
   ruby_gem { 'tmuxinator':
     gem          => 'tmuxinator',
-    ruby_version => '*'
-  }
-
-  ruby_gem { 'gemedit':
-    gem          => 'gemedit',
+    version      => '~> 0.6',
     ruby_version => '*'
   }
 
   ruby_gem { 'rubocop':
     gem          => 'rubocop',
+    version      => '~> 0.24',
     ruby_version => '*'
   }
 
   ruby_gem { 'timetrap':
     gem          => 'timetrap',
+    version      => '~> 1.8',
     ruby_version => '*'
   }
 
