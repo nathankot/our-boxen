ca85ef95fc59a51ff1e125d5656b06003eb94dbb
Nathan Kot <me@nathankot.com>
2014-08-10 15:33:38 +1200

    Dont use heroku wildcards.

--- modules/people/manifests/nathankot.pp
+++ modules/people/manifests/nathankot.pp
@@ -62,10 +62,6 @@ class people::nathankot {
     source => 'https://github.com/tpope/heroku-binstubs.git'
   }
 
-  heroku::plugin { 'wildcards':
-    source => 'https://github.com/tpope/heroku-wildcards.git'
-  }
-
   heroku::plugin { 'repo':
     source => 'https://github.com/heroku/heroku-repo.git'
   }
