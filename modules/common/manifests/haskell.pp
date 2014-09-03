ea57baa839e7d05d6eaba709aaa6ddc2e91e488d
Nathan Kot <me@nathankot.com>
2014-09-03 23:45:36 +1200

    Cabal packages.

--- modules/common/manifests/haskell.pp
+++ modules/common/manifests/haskell.pp
@@ -1,6 +1,32 @@
 class common::haskell {
+  include haskell::platform
+  haskell::cabal { 'hdevtools': }
+  haskell::cabal { 'ghc-mod': }
+}
 
+class haskell::platform {
   require homebrew
   package { 'haskell-platform': }
+}
 
+define haskell::cabal (
+  $ensure = present
+) {
+  require haskell::platform
+
+  case $ensure {
+    present: {
+      exec { "cabal_install_${name}":
+        command => "cabal install ${name}",
+        unless  => "cabal list --simple-output --installed | grep -i -e ^${name}\$",
+      }
+    }
+
+    default: {
+      exec { "cabal_uninstall_${name}":
+        command => "cabal uninstall ${name}",
+        onlyif  => "cabal list --simple-output --installed | grep -i -e ^${name}\$",
+      }
+    }
+  }
 }
