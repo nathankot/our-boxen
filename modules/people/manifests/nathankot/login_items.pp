6722aa60b089f3ce9e42ee9cc5d58de8f00ffa0b
Nathan Kot <me@nathankot.com>
2014-09-12 11:18:50 +1200

    More login items.

--- modules/people/manifests/nathankot/login_items.pp
+++ modules/people/manifests/nathankot/login_items.pp
@@ -12,4 +12,19 @@ class people::nathankot::login_items {
     path => '/Applications/Flux.app'
   }
 
+  osx_login_item { 'iTerm':
+    name => 'iTerm',
+    path => '/Applications/iTerm.app'
+  }
+
+  osx_login_item { 'Dash':
+    name => 'Dash',
+    path => '/Applications/Dash.app'
+  }
+
+  osx_login_item { 'Mjolnir':
+    name => 'Mjolnir',
+    path => '/Applications/Mjolnir.app'
+  }
+
 }
