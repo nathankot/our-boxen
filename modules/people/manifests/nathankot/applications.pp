a91fe02a1c5ad7d6cf77bad74053c2937983ddb5
Nathan Kot <me@nathankot.com>
2014-09-11 21:06:30 +1200

    Gonna use dash.

--- modules/people/manifests/nathankot/applications.pp
+++ modules/people/manifests/nathankot/applications.pp
@@ -2,6 +2,7 @@ class people::nathankot::applications {
   include flux
   include shortcat
   include transmission
+  include dash
   include global::applications::amethyst
   include global::applications::mjolnir
   include global::applications::kaleidoscope
