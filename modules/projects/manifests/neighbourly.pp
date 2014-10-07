d8c132fb12b5909acefa90e9c41e58fb76e9f854
Nathan Kot <me@nathankot.com>
2014-10-08 10:44:09 +1300

    Neighbourly - manage own php.

--- modules/projects/manifests/neighbourly.pp
+++ modules/projects/manifests/neighbourly.pp
@@ -7,12 +7,6 @@ class projects::neighbourly {
 
   package { 'elasticsearch11': ensure  => installed }
 
-  $version = '5.4.24'
-
-  php::local { "/Users/${::boxen_user}/Development/Sites/neighbourly":
-    version => $version
-  }
-
   boxen::project { 'neighbourly':
     mysql   => [],
     source  => 'shophq/neighbourly',
