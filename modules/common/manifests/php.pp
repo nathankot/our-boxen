f7e78f3432de327443c7ee032df3653637a7e9cd
Nathan Kot <me@nathankot.com>
2014-10-08 14:01:49 +1300

    Use own version of php.

--- modules/common/manifests/php.pp
+++ modules/common/manifests/php.pp
@@ -5,25 +5,11 @@ class common::php {
   homebrew::tap { 'homebrew/homebrew-php':  }
 
   include php::5_4
-  include php::5_5
+  include php::5_5_16
   include php::composer
   include memcached
 
   package { 'imagemagick': ensure => installed  }
-
-  $version = '5.4.24'
-
-  php::extension::intl { "intl for ${version}":
-    php =>  $version
-  }
-
-  php::extension::memcache { "memcache for ${version}":
-    php =>  $version
-  }
-
-  php::extension::xdebug { "xdebug for ${version}":
-    php =>  $version
-  }
 
   package { 'phpunit':
     ensure =>  installed
--- modules/projects/manifests/neighbourly.pp
+++ modules/projects/manifests/neighbourly.pp
@@ -7,6 +7,16 @@ class projects::neighbourly {
 
   package { 'elasticsearch11': ensure  => installed }
 
+  $version = '5.5.0'
+
+  php::local { "/Users/${::boxen_user}/Development/Sites/neighbourly":
+    version => $version
+  }
+
+  php::extension::memcache { "memcache for ${version}":
+    php =>  $version
+  }
+
   boxen::project { 'neighbourly':
     mysql   => [],
     source  => 'shophq/neighbourly',
