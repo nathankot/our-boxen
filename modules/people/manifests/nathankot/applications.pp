7889942ccda22a748ce7ef5d673b11a8d1e80f3e
Nathan Kot <me@nathankot.com>
2014-10-24 15:59:22 +1300

    Don't need amethyst not more.

--- modules/people/manifests/nathankot/applications.pp
+++ modules/people/manifests/nathankot/applications.pp
@@ -4,7 +4,6 @@ class people::nathankot::applications {
   include transmission
   include dash
   include spotify
-  include global::applications::amethyst
   include global::applications::mjolnir
   include global::applications::kaleidoscope
   include global::applications::mongohub
