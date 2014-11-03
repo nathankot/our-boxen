4f986860ba4db5d795926c7280b7b72fdb622a56
Nathan Kot <me@nathankot.com>
2014-11-03 20:14:55 +0900

    Graphicsmagick for wedis

--- modules/projects/manifests/wedis.pp
+++ modules/projects/manifests/wedis.pp
@@ -3,6 +3,8 @@ class projects::wedis {
   require people::nathankot::folders
   include heroku
 
+  package { 'graphicsmagick': ensure => installed  }
+
   boxen::project { 'wed.is':
     nodejs  => 'v0.10',
     source  => 'nathankot/wed.is',
