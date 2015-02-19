6e4d7bda181421bada8721a4eadce316b3e62ba0
Nathan Kot <me@nathankot.com>
2015-02-20 10:19:14 +1300

    Install new cabal-install first.

--- modules/common/manifests/haskell.pp
+++ modules/common/manifests/haskell.pp
@@ -2,7 +2,6 @@ class common::haskell {
   include haskell::platform
   haskell::cabal { 'hdevtools': }
   haskell::cabal { 'ghc-mod': }
-  haskell::cabal { 'cabal-install': }
   haskell::cabal { 'hledger': }
   haskell::cabal { 'hoogle': }
   haskell::cabal { 'hspec': }
@@ -11,19 +10,17 @@ class common::haskell {
 # STUBBED FOR NOW. If you're getting cabal install errors please 
 # install haskell via their package first: https://www.haskell.org/platform/mac.html
 class haskell::platform {
-  require homebrew
+  haskell::cabal { 'cabal-install': }
 }
 
 # Allows requiring cabal packages, yay.
 define haskell::cabal (
   $ensure = present
 ) {
-  require haskell::platform
-
   case $ensure {
     present: {
       exec { "cabal_install_${name}":
-        command => "cabal install ${name}",
+        command => "cabal update; cabal install ${name}",
         unless  => "cabal list --simple-output --installed | grep -i -e ^${name}\$",
       }
     }
