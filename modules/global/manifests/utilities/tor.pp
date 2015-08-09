1165a893a8afc5232f3c3c4b3a4f41dcedf2eeb3
Nathan Kot <me@nathankot.com>
2015-08-10 01:32:47 +0900

    Use stock tor plist

--- modules/global/manifests/utilities/tor.pp
+++ modules/global/manifests/utilities/tor.pp
@@ -6,15 +6,8 @@ class global::utilities::tor {
     notify => Service['tor']
   }
 
-  file { '/Library/LaunchAgents/dev.tor.plist':
-    content => template('global/dev.tor.plist.erb'),
-    group => 'wheel',
-    owner => 'root',
-    notify => Service['tor']
-  }
-
   service { 'tor':
-    require => File["/Library/LaunchAgents/dev.tor.plist"],
+    require => File["/Users/${::boxen_user}/Library/LaunchAgents/homebrew.mxcl.tor.plist"],
     ensure => running
   }
 }
