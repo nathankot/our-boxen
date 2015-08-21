9eab8e0edf84893744d89fd6e32c6679171743f1
Nathan Kot <me@nathankot.com>
2015-08-21 21:38:27 +0900

    Remove some weird karabiner settings

--- modules/common/manifests/config/karabiner/remap.pp
+++ modules/common/manifests/config/karabiner/remap.pp
@@ -5,11 +5,9 @@ class common::config::karabiner::remap {
   karabiner::remap { 'private.tmux_simultaneous': }
   karabiner::remap { 'notsave.tab_numpad_numbers': }
   karabiner::remap { 'private.tab_numpad': }
-  karabiner::remap { 'notsave.automatically_enable_pointing_device': }
   karabiner::remap { 'remap.escape2backquote': }
   karabiner::remap { 'remap.controlL2controlL_escape': }
   karabiner::remap { 'private.vim_on_f': }
   karabiner::remap { 'remap.shiftparens': }
-  karabiner::remap { 'notsave.automatically_enable_keyboard_device': }
 
 }
