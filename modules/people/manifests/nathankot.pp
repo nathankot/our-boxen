261eb1b2a4d75dd541e6d359ee451e73ccf09738
Nathan Kot <me@nathankot.com>
2016-06-10 11:33:29 -0700

    Revert "Install ghi"
    This reverts commit bab23bdfa2e0b6194eabfc1d1d6008b98b96ec60.

--- modules/people/manifests/nathankot.pp
+++ modules/people/manifests/nathankot.pp
@@ -56,7 +56,6 @@ class people::nathankot {
   package { 'x264': ensure => installed }
   package { 'jq': ensure => installed }
   package { 'spark': ensure => installed }
-  package { 'ghi': ensure => installed }
 
   package { 'docker': ensure => installed }
   package { 'docker-compose': ensure => installed }
