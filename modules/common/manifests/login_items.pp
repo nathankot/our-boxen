d832da9289d795d2475890678d6869e70c7258d8
Nathan Kot <me@nathankot.com>
2016-11-28 18:00:06 +0900

    Remove dropbox as a login item

--- modules/common/manifests/login_items.pp
+++ modules/common/manifests/login_items.pp
@@ -1,10 +1,5 @@
 class common::login_items {
 
-  osx_login_item { 'Dropbox':
-    name => 'Dropbox',
-    path => '/Applications/Dropbox.app'
-  }
-
   osx_login_item { 'Google Drive':
     name => 'Google Drive',
     path => '/Applications/Google\ Drive.app'
