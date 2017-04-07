aaeb78cfa431fecf973c47ac172dd3e65144f84b
Nathan Kot <me@nathankot.com>
2017-04-08 02:22:43 +1200

    Setup additional login items

--- modules/people/manifests/nathankot/login_items.pp
+++ modules/people/manifests/nathankot/login_items.pp
@@ -5,4 +5,19 @@ class people::nathankot::login_items {
     path => '/Applications/Shortcat.app'
   }
 
+  osx_login_item { 'Hammerspoon':
+    name => 'Hammerspoon',
+    path => '/Applications/Hammerspoon.app'
+  }
+
+  osx_login_item { 'Karabiner Elements':
+    name => 'Karabiner Elements',
+    path => '/Applications/Karabiner-Elements.app'
+  }
+
+  osx_login_item { 'Übersicht':
+    name => 'Übersicht',
+    path => '/Applications/Übersicht.app'
+  }
+
 }
