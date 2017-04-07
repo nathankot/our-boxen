a25380b1878484cc5ce1951fde8bdc2224932a8f
Nathan Kot <me@nathankot.com>
2017-04-08 02:31:36 +1200

    Fix login item for Google Drive

--- modules/common/manifests/login_items.pp
+++ modules/common/manifests/login_items.pp
@@ -2,7 +2,7 @@ class common::login_items {
 
   osx_login_item { 'Google Drive':
     name => 'Google Drive',
-    path => '/Applications/Google\ Drive.app'
+    path => '/Applications/Google Drive.app'
   }
 
 }
